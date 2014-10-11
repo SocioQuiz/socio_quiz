9a7324b567bd4ff7943f06b26f8bb73c38111a32
Praween AMONTAMAVUT <not001praween001@gmail.com>
2014-10-11 19:43:27 +0900

    Add AuthHash inspector.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,6 @@
 OmniAuth.config.on_failure do |env|
 	[200, {}, [env['omniauth.error'].inspect]]
+	[200, {}, [env['omniauth.auth'].inspect]]
 end
 Rails.application.config.middleware.use OmniAuth::Builder do
 	  provider :facebook, ENV['FACEBOOK_APP_KEY'], ENV['FACEBOOK_APP_SECRET'],
