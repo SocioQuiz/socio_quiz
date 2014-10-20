468751c39156aceeddaeae7de76fabdabbb02cc6
Praween AMONTAMAVUT <not001praween001@gmail.com>
2014-10-21 02:36:17 +0900

    Run migrated db again in development fork repo.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141020081215) do
+ActiveRecord::Schema.define(version: 20141020083106) do
 
   create_table "quizzes", force: true do |t|
     t.string   "question"
