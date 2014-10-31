be7eafa799cd7eccc30487d1b305d9d8608b6466
harakoji <harakoji@harakoji.net>
2014-10-31 05:25:40 +0000

    judging users_s answer is correct.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141030130717) do
+ActiveRecord::Schema.define(version: 20141030160334) do
 
   create_table "oauth_access_grants", force: true do |t|
     t.integer  "resource_owner_id", null: false
@@ -59,6 +59,7 @@ ActiveRecord::Schema.define(version: 20141030130717) do
     t.integer  "score"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "reply"
   end
 
   create_table "quizzes", force: true do |t|
