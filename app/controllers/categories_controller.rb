222d4cfcef12b2e773735f3b6eaca6e318e81b32
Praween AMONTAMAVUT <not001praween001@gmail.com>
2014-12-05 13:32:02 +0900

    add authenticate_user filtering.

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,7 @@
 class CategoriesController < ApplicationController
-  before_filter :authenticate_user!
   before_action :set_category, only: [:show, :edit, :update, :destroy]
+  before_filter :authenticate_user!, only: [:new, :edit, :update, :destroy]
+
   respond_to :html, :xml
 
   def index
